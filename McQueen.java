709331240c5f4d7d0db16ddbd904f0b2446ad439
Soren Fey <sfey25@students.polytechnic.org>
2024-03-05 12:23:22 -0800

    work on McQueen

--- McQueen.java
+++ McQueen.java
@@ -1,10 +1,22 @@
-/**
- * Makes the legendary McQueen
- */
+import java.util.Random;
 
-public class McQueen extends AbstractRacer {
-    private double chanceOfCrash;
-    public void move() {
-        //move the car 
+public class McQueen extends AbstractRacer 
+{
+    public McQueen(double location) 
+    {
+        super(location);
+    }
+    public void move() 
+    {
+        Random rand = new Random();
+        double randNum = rand.nextDouble(11);
+        if (randNum < 8.0)
+        {
+            location += randNum;
+        }
+        else
+        {
+            location = 0;
+        }
     }
 }
